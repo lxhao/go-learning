440f8612112203bb0f2ab2447ab285b580967daf
Your Name <you@example.com>
2022-06-12 08:59:54 +0800

    lock

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lxhao/go-learning
+
+go 1.18
---
+++ llock/mutex_lock_test.go
@@ -0,0 +1,90 @@
+package llock
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var (
+	// 逻辑中使用的某个变量
+	count int
+	// 与变量对应的使用互斥锁
+	countGuard sync.Mutex
+)
+
+func Increment(useLock bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if useLock {
+		countGuard.Lock()
+		defer countGuard.Unlock()
+	}
+	count++
+}
+
+func TestMutex(t *testing.T) {
+	// 并发去更新
+	var wg sync.WaitGroup //定义一个同步等待的组
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go Increment(false, &wg)
+	}
+	wg.Wait()
+	// 多运行几次，这里的结果不会是100
+	fmt.Printf("没有使用锁的时候，并发更新的结果%d\n", count)
+
+	count = 0
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go Increment(true, &wg)
+	}
+	wg.Wait()
+	fmt.Printf("使用锁的时候，并发更新的结果%d\n", count)
+}
+
+func TestRWMutex(t *testing.T) {
+	var rwMutex sync.RWMutex
+
+	var wg sync.WaitGroup //定义一个同步等待的组
+	wg.Add(1)
+	// 协程1加读锁
+	go func() {
+		rwMutex.Lock()
+		fmt.Println("协程1加读锁")
+		defer rwMutex.Unlock()
+		time.Sleep(5 * time.Second)
+		wg.Done()
+	}()
+
+	// 等1下，确保协程1已经加锁
+	time.Sleep(1 * time.Second)
+	wg.Add(1)
+	// 协程2加读锁
+	go func() {
+		if rwMutex.TryLock() {
+			fmt.Println("协程2加读锁成功")
+			defer rwMutex.Lock()
+			time.Sleep(5 * time.Second)
+		} else {
+			t.Error("协程2加读锁失败")
+		}
+		wg.Done()
+	}()
+
+	// 等1下，确保协程2已经加锁
+	// 协程3加写锁
+	time.Sleep(1 * time.Second)
+	wg.Add(1)
+	go func() {
+		if rwMutex.TryRLock() {
+			fmt.Println("协程3加写锁成功")
+			defer rwMutex.RLock()
+			time.Sleep(5 * time.Second)
+		} else {
+			fmt.Println("协程3加写锁失败")
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
